1df463c25889d1e05f7012070ddb9870e70555c5
Ahmad Bilal <40148920+billyboyballin@users.noreply.github.com>
2021-01-18 14:19:06 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 
 ############################### Prerequisites ###############################
+install_awscli () {
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+}
 
 install_cfssl () {
   wget -q --show-progress --https-only --timestamping \
