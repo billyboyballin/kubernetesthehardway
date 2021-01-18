98259aeae2c9377573b536ae3cff3165988df6b6
Ahmad Bilal <40148920+billyboyballin@users.noreply.github.com>
2021-01-18 14:21:45 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 ############################### Prerequisites ###############################
 install_awscli () {
+  sudo apt install unzip
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
